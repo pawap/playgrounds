aeed152541510e5198c357d7aff5c1771624d622
DESKTOP-12KGL0N\wps <pawel.rasch@wps.de>
2020-10-13 00:05:31 +0200

    [PawProj] Cleanup dead code

--- src/main/resources/static/js/misc.js
+++
@@ -1,8 +0,0 @@
-		<script th:inline="javascript">
-  			var features = [];
-		</script>
-		<script th:each="playground: ${playgrounds}" th:inline="javascript">
-  			features.push(new ol.Feature({
-                   geometry: new ol.geom.Point(ol.proj.fromLonLat([[${playground.longitude}]], [[${playground.latitude}]]))
-             }));
-		</script>
